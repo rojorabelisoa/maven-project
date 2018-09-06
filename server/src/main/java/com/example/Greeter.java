359a21e1fd9012f353a648d374527d584963b076
rojorabelisoa <rojorabelisoa@yahoo.fr>
2018-09-06 15:24:54 +0200

    fix comment
    fix comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,3 +1,6 @@
+/**
+* this is default package
+**/
 package com.example;
 
 /**
@@ -12,7 +15,10 @@ public class Greeter {
 
   }
 
- //TODO: Add javadoc comment
+ /**
+   * @param someone name of person
+   * @return greeting return
+   */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
